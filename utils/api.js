a8fea8369c24b103c283c2c1139c2a6cb465c31d
Deskola <denisogunde@gmail.com>
2022-05-22 18:44:44 +0300

    React-Native Weather App

--- utils/api.js
+++ utils/api.js
@@ -1,25 +1,3 @@
-export const fetchLocationId = async city => {
-  const response = await fetch(
-    `https://www.metaweather.com/api/location/search/?query=${city}`,
-  );
-  const locations = await response.json();
-  return locations[0].woeid;
-};
-
-export const fetchWeather = async woeid => {
-  const response = await fetch(
-    `https://www.metaweather.com/api/location/${woeid}/`,
-  );
-  const { title, consolidated_weather } = await response.json();
-  const { weather_state_name, the_temp } = consolidated_weather[0];
-
-  return {
-    location: title,
-    weather: weather_state_name,
-    temperature: the_temp,
-  };
-};
-
 export const fetchWeatherInfo = async city => {
   const response = await fetch(
     `http://api.weatherstack.com/current?access_key=<YOUR API KEY>&query=${city}`
